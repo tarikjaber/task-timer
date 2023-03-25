adb7c4c0fb97bcec3d4bce334de38a53d38b1b60
Tarik Jaber <tariknjaber@gmail.com>
2023-03-25 00:38:29 -0400

    Add pause functionality

--- index.js
+++ index.js
@@ -1,100 +1,92 @@
 const taskInput = document.getElementById("task-input");
 const startButton = document.getElementById("start-button");
-const timer = document.getElementById("timer");
 const title = document.getElementById("title");
 
-let taskName = "";
-let taskTime = 0;
-let currentTask = "";
-let nextTask = "";
+let currentTaskIndex = 0;
+let tasks = [];
 let timerId;
 let audio = new Audio('ding.mp3');
-let isRunning = false;
+let remainingTime = 0; // new variable to store remaining time for the current task
 
 function startTimer() {
-    // Clear any existing timer
     clearInterval(timerId);
-
-    // Get task names and times from input
-    const inputText = taskInput.value.trim();
-    if (inputText === "") {
+    if (tasks.length === 0) {
+        const tasksArray = taskInput.value.trim().split('\n');
+        for (let i = 0; i < tasksArray.length; i += 2) {
+            const taskName = tasksArray[i];
+            const taskTime = tasksArray[i + 1];
+            tasks.push({ name: taskName, time: taskTime });
+        }
+        console.log(tasks)
+    }
+    if (tasks.length === 0) {
         return;
     }
-    const inputArray = inputText.split("\n");
-
-    // Loop through tasks
-    let currentTaskIndex = 0;
-    const totalTasks = inputArray.length / 2;
-    const startNextTask = () => {
-        if (currentTaskIndex < totalTasks) {
-            // Set current and next task names and times
-            currentTask = inputArray[currentTaskIndex * 2].trim();
-            taskTime = parseInt(inputArray[currentTaskIndex * 2 + 1].trim());
-            nextTask = inputArray[currentTaskIndex * 2 + 2]?.trim() || "";
-            let timeLeft = 0; // Add this variable to store dthe remaining time
-
-            // If the timer is being resumed, start from the remaining time
-            if (timeLeft > 0) {
-                taskTime = Math.floor(timeLeft / 60);
-                timeLeft = timeLeft % 60;
-            } else {
-                timeLeft = taskTime * 60;
-            }
-
-            timerId = setInterval(() => {
-                if (!isRunning) {
-                    return;
-                }
-                timeLeft--;
-                const minutesLeft = Math.floor(timeLeft / 60);
-                const secondsLeft = timeLeft % 60;
-                title.textContent = `${currentTask} - ${minutesLeft}:${secondsLeft.toString().padStart(2, "0")}`;
-
-                // Update page title with task name and remaining time
-                document.title = `${currentTask} - ${minutesLeft}:${secondsLeft.toString().padStart(2, "0")}`;
-
-                // Check if time is up
-                if (timeLeft === 0) {
-                    clearInterval(timerId);
-                    // Set title to next task name
-                    title.textContent = `Task Timer`;
-                    // Send notification
-                    const notification = `${currentTask} completed, ${nextTask} started for ${taskTime} minutes`;
-                    // Start next task
-                    currentTaskIndex++;
-                    audio.play();
-                    if (currentTaskIndex < totalTasks) {
-                        new Notification(notification);
-                        startNextTask();
-                    } else {
-                        new Notification("All tasks completed!")
-                        taskInput.value = ""
-                        taskInput.focus()
-                        document.title = "Task Timer";
-                    }
-                }
-            }, 1000);
-
-            isRunning = true;
-        } else {
-            // All tasks are completed, reset timer
-            clearInterval(timerId);
-            title.textContent = "Task Timer";
-            document.title = "Task Timer";
-            taskInput.value = "";
-            taskInput.focus();
-            isRunning = false;
+    if (startButton.textContent === 'Play') {
+        startButton.textContent = 'Pause';
+        startButton.style.backgroundColor = "#FE6244";
+        if (currentTaskIndex >= tasks.length) {
+            currentTaskIndex = 0;
         }
-    };
-
-    startNextTask();
+        startNextTask();
+    } else {
+        startButton.textContent = 'Play';
+        startButton.style.backgroundColor = "#57C5B6";
+        clearInterval(timerId);
+    }
 }
 
+function startNextTask() {
+    if (tasks.length === 0) {
+        return;
+    }
+    const task = tasks[currentTaskIndex];
+    if (remainingTime) { // if there is remaining time for the current task, use it
+        var timeLeft = remainingTime;
+        remainingTime = 0;
+    } else { // otherwise start new task
+        var timeLeft = task.time * 60;
+    }
+    timerId = setInterval(() => {
+        if (startButton.textContent === 'Play') { // use '===' instead of '!==' here
+            return;
+        }
+        timeLeft--;
+        remainingTime = timeLeft; // store remaining time for the current task
+        const minutesLeft = Math.floor(timeLeft / 60);
+        const secondsLeft = timeLeft % 60;
+        title.textContent = `${task.name} - ${minutesLeft}:${secondsLeft.toString().padStart(2, "0")}`;
+        document.title = `${task.name} - ${minutesLeft}:${secondsLeft.toString().padStart(2, "0")}`;
+        if (timeLeft === 0) {
+            clearInterval(timerId);
+            const notification = new Notification(`${task.name} completed, ${tasks[currentTaskIndex + 1]?.name} started for ${task.time} minutes`);
+            currentTaskIndex++;
+            audio.play();
+            if (currentTaskIndex < tasks.length) {
+                notification;
+                startNextTask();
+            } else {
+                new Notification("All tasks completed!")
+                startButton.textContent = "Play";
+                tasks = [];
+                taskInput.value = "";
+                title.textContent = "Task Timer";
+                document.title = "Task Timer";
+            }
+        }
+    }, 1000);
+}
 
 function handleKeyPress(event) {
     if (event.shiftKey && event.key === "Enter") {
         event.preventDefault();
         startTimer();
+    } else {
+        remainingTime = 0; // reset remaining time for the current task
+        clearInterval(timerId);
+        startButton.textContent = "Play";
+        startButton.style.backgroundColor = "#57C5B6";
+        tasks = [];
     }
 }
 
